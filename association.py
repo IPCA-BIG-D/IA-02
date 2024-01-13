8db0615f1e223142740ad2fc933283c7a1518c90
Alexandre Pereira <snivypt@gmail.com>
2024-01-13 18:08:50 +0000

    adicionar comentários

--- association.py
+++ association.py
@@ -2,47 +2,48 @@ import pandas as pd
 from mlxtend.frequent_patterns import apriori, association_rules
 import random
 
-# Load the MovieLens Small Latest Dataset
-# Replace 'path/to/movies.csv' and 'path/to/ratings.csv' with the actual paths to your downloaded files
+# Path para os dados do dataset - MovieLens Small Latest Dataset (https://www.kaggle.com/datasets/shubhammehta21/movie-lens-small-latest-dataset)
 movies_path = './movies_input/movies.csv'
 ratings_path = './movies_input/ratings.csv'
 
+# Lê os dados
 movies = pd.read_csv(movies_path)
 ratings = pd.read_csv(ratings_path)
 
-# Merge movies and ratings data
+# Junta os ratings com os filmes
 data = pd.merge(ratings, movies, on='movieId')
 
-# Filter movies rated higher than a certain threshold (e.g., 4)
+# Filtra os filmes com um rating superior ou igual a "threshold"
 threshold = 4
 data = data[data['rating'] >= threshold]
 
-# Sample a random subset of users
-random.seed(42)  # Set a seed for reproducibility
-user_subset = random.sample(data['userId'].unique().tolist(), k=min(1000, len(data['userId'].unique()))) #mudar o 1000
+random.seed(42)  # Definir uma semente para reprodutibilidade
+
+# Amostragem de utilizadores aleatória
+user_subset = random.sample(data['userId'].unique().tolist(), k=min(1000, len(data['userId'].unique()))) # 1000 - sub conjunto de dados a utilizar
 data_subset = data[data['userId'].isin(user_subset)]
 
-# Sample a random subset of movies
-movie_subset = random.sample(data_subset['movieId'].unique().tolist(), k=min(100, len(data_subset['movieId'].unique()))) #mudar o 100
+# Amostragem de filmes aleatória
+movie_subset = random.sample(data_subset['movieId'].unique().tolist(), k=min(100, len(data_subset['movieId'].unique()))) # 100 - sub conjunto de dados a utilizar
 data_subset = data_subset[data_subset['movieId'].isin(movie_subset)]
 
-# Aggregate ratings for duplicate entries
+# Agregar ratings de entradas duplicadas
 data_subset = data_subset.groupby(['userId', 'title'])['rating'].max().reset_index()
 
-# Create a binary matrix representing user-movie preferences
+# Cria uma matriz de preferência utilizador-filme
 user_movie_matrix = data_subset.pivot(index='userId', columns='title', values='rating').fillna(0)
 
-# Convert ratings to binary values (1 or 0)
+# Converter ratings para valores binários
 basket_sets = user_movie_matrix.applymap(lambda x: 1 if x >= threshold else 0)
 
-# Generate frequent itemsets using Apriori
+# Gerar conjuntos frequentes usando o algoritmo Apriori
 min_support = 0.01
 frequent_itemsets = apriori(basket_sets, min_support=min_support, use_colnames=True)
 
-# Generate association rules
+# Gerar regras de associação
 rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.5)
 
-# Print the association rules
+# Apresenta as regras
 for index, rule in rules.iterrows():
     antecedent_movies = rule['antecedents']
     consequent_movies = rule['consequents']
@@ -53,3 +54,7 @@ for index, rule in rules.iterrows():
     print(f"  Confidence: {rule['confidence']:.2f}")
     print(f"  Lift: {rule['lift']:.2f}")
     print("-" * 50)
+
+# If Lift = 1: The antecedent and consequent are independent, and knowing one doesn't provide any information about the other.
+# If Lift > 1: The antecedent and consequent are positively correlated. The higher the lift, the stronger the correlation.
+# If Lift < 1: The antecedent and consequent are negatively correlated. A lift less than 1 indicates that the items are less likely to occur together than if they were independent.
--- main.py
+++ main.py
@@ -9,12 +9,13 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.cluster import KMeans
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-# Load the dataset
-# url = "https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data"
+# Declarar colunas do dataset
 column_names = ["class", "cap-shape", "cap-surface", "cap-color", "bruises", "odor", "gill-attachment",
                 "gill-spacing", "gill-size", "gill-color", "stalk-shape", "stalk-root", "stalk-surface-above-ring",
                 "stalk-surface-below-ring", "stalk-color-above-ring", "stalk-color-below-ring", "veil-type",
                 "veil-color", "ring-number", "ring-type", "spore-print-color", "population", "habitat"]
+
+# Lê dados do dataset
 mushroom_data = pd.read_csv('./input/mushrooms.csv', header=None, names=column_names)
 
 # Encode categorical features
@@ -22,67 +23,78 @@ le = LabelEncoder()
 for column in mushroom_data.columns:
     mushroom_data[column] = le.fit_transform(mushroom_data[column])
 
-# Split the data into features (X) and target variable (y)
+# Separar os dados em features (X) e objetivo (y)
 X = mushroom_data.drop('class', axis=1)
 y = mushroom_data['class']
 
-# Split the data into training and testing sets
+# Dividir os dados em dataset de treino e teste
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# K-Means Clustering
-kmeans = KMeans(n_clusters=4, random_state=42)
-X_train_clustered = kmeans.fit_predict(X_train)
+# -------------------------------------------- Decision Tree  --------------------------------------------
 
 # Decision Tree Classifier
 # dt_clf = DecisionTreeClassifier(random_state=42)
-dt_clf = DecisionTreeClassifier(max_depth=5, min_samples_split=2, min_samples_leaf=1, random_state=42)
+dt_clf = DecisionTreeClassifier(max_depth=5, min_samples_split=2, min_samples_leaf=1, random_state=42) # amostragem
 dt_clf.fit(X_train, y_train)
 dt_y_pred = dt_clf.predict(X_test)
 
-# RandomForest Classifier
-# rf_clf = RandomForestClassifier(random_state=42)
-rf_clf = RandomForestClassifier(n_estimators=100, max_depth=10, min_samples_split=2, min_samples_leaf=1, random_state=42)
-rf_clf.fit(X_train, y_train)
-rf_y_pred = rf_clf.predict(X_test)
-
-# Use K-Means clusters for testing set
-X_test_clustered = kmeans.predict(X_test)
-
-# Evaluate the Decision Tree classifier
+# Avalia a Decision Tree classifier
 dt_accuracy = accuracy_score(y_test, dt_y_pred)
 dt_conf_matrix = confusion_matrix(y_test, dt_y_pred)
 dt_class_report = classification_report(y_test, dt_y_pred)
 
-# Evaluate the RandomForest classifier
-rf_accuracy = accuracy_score(y_test, rf_y_pred)
-rf_conf_matrix = confusion_matrix(y_test, rf_y_pred)
-rf_class_report = classification_report(y_test, rf_y_pred)
-
-# Evaluate K-Means clusters
-kmeans_accuracy = accuracy_score(y_test, X_test_clustered)
-kmeans_conf_matrix = confusion_matrix(y_test, X_test_clustered)
-
-# Print the results for Decision Tree
+# Apresenta resultados Decision Tree
 print("Decision Tree Classifier Results:")
 print("Accuracy:", dt_accuracy)
 print("Confusion Matrix:\n", dt_conf_matrix)
 print("Classification Report:\n", dt_class_report)
 
-# Print the results for RandomForest
+# -------------------------------------------------------------------------------------------------
+
+# -------------------------------------------- RandomForest --------------------------------------------
+
+# RandomForest Classifier
+# rf_clf = RandomForestClassifier(random_state=42)
+rf_clf = RandomForestClassifier(n_estimators=100, max_depth=10, min_samples_split=2, min_samples_leaf=1, random_state=42) # amostragem
+rf_clf.fit(X_train, y_train)
+rf_y_pred = rf_clf.predict(X_test)
+# Avalia o RandomForest classifier
+rf_accuracy = accuracy_score(y_test, rf_y_pred)
+rf_conf_matrix = confusion_matrix(y_test, rf_y_pred)
+rf_class_report = classification_report(y_test, rf_y_pred)
+
+# Apresenta resultados RandomForest
 print("\nRandomForest Classifier Results:")
 print("Accuracy:", rf_accuracy)
 print("Confusion Matrix:\n", rf_conf_matrix)
 print("Classification Report:\n", rf_class_report)
 
-# Print the results for K-Means clustering
+# -------------------------------------------------------------------------------------------------
+
+# -------------------------------------------- K-means --------------------------------------------
+
+# K-Means Clustering
+kmeans = KMeans(n_clusters=4, random_state=42) # amostragem
+X_train_clustered = kmeans.fit_predict(X_train)
+
+# Usa K-Means para testar o conjunto 
+X_test_clustered = kmeans.predict(X_test)
+
+# Avalia K-Means clusters
+kmeans_accuracy = accuracy_score(y_test, X_test_clustered)
+kmeans_conf_matrix = confusion_matrix(y_test, X_test_clustered)
+
+# Apresenta resultados K-Means clustering
 print("\nK-Means Clustering Results:")
 print("Accuracy:", kmeans_accuracy)
 print("Confusion Matrix:\n", kmeans_conf_matrix)
 
+# -------------------------------------------------------------------------------------------------
+
 # Graphs region
 
 # Visualizations
-fig, axes = plt.subplots(2, 3, figsize=(18, 10))
+fig, axes = plt.subplots(2, 2, figsize=(18, 10))
 
 # Decision Tree Confusion Matrix
 sns.heatmap(dt_conf_matrix, annot=True, fmt='d', cmap='Blues', ax=axes[0, 0])
@@ -93,24 +105,17 @@ sns.heatmap(rf_conf_matrix, annot=True, fmt='d', cmap='Greens', ax=axes[0, 1])
 axes[0, 1].set_title('RandomForest Confusion Matrix')
 
 # K-Means Confusion Matrix
-sns.heatmap(confusion_matrix(y_test, X_test_clustered), annot=True, fmt='d', cmap='Reds', ax=axes[0, 2])
-axes[0, 2].set_title('K-Means Confusion Matrix')
+sns.heatmap(confusion_matrix(y_test, X_test_clustered), annot=True, fmt='d', cmap='Reds', ax=axes[1, 0])
+axes[1, 0].set_title('K-Means Confusion Matrix')
 
-# Scatter plot for K-Means Clusters (using the first two features) with a sample amount of 500
+# Scatter plot for K-Means Clusters (utilizando as 2 primeiras features(X)) com uma amostragem de "sample_size"
 sample_size = 1000
+
 sample_indices = np.random.choice(X_test.shape[0], sample_size, replace=False)
-axes[1, 0].scatter(X_test.iloc[sample_indices, 0], X_test.iloc[sample_indices, 1], c=X_test_clustered[sample_indices], cmap='viridis')
-axes[1, 0].scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], marker='X', s=200, c='red', label='Centroids')
-axes[1, 0].set_title(f'K-Means Clustering (Sampled {sample_size} points)')
-axes[1, 0].legend()
-
-# Scatter plot for RandomForest Classifier (using dots) with a sample amount of 500
-axes[1, 1].scatter(X_test.iloc[sample_indices, 0], X_test.iloc[sample_indices, 1], c=rf_y_pred[sample_indices], cmap='viridis', marker='.')
-axes[1, 1].set_title(f'RandomForest Classifier (Scatter Plot) (Sampled {sample_size} points)')
-
-# Class Distributions
-sns.countplot(x=y_test, ax=axes[1, 2])
-axes[1, 2].set_title('Class Distributions')
+axes[1, 1].scatter(X_test.iloc[sample_indices, 0], X_test.iloc[sample_indices, 1], c=X_test_clustered[sample_indices], cmap='viridis')
+axes[1, 1].scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], marker='X', s=200, c='red', label='Centroids')
+axes[1, 1].set_title(f'K-Means Clustering (Sampled {sample_size} points)')
+axes[1, 1].legend()
 
 plt.tight_layout()
 plt.show()
